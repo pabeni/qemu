e980ab6935f910f287df015fb23ba5d80dbeeaaf
Paolo Abeni <pabeni@redhat.com>
2025-05-09 19:20:22 +0200

    virtio: introduce extended features type
    The virtio specifications allows for up to 128 bits for the
    device features. Soon we are going to use some of the 'extended'
    bits features (above 64) for the virtio net driver.
    
    Introduce a specific type to represent the virtio features bitmask.
    On platform where 128 bits integer are available use such wide int
    for the features bitmask, otherwise maintain the current u64.
    
    Most drivers will keep using only 64 bits features space; use union
    to allow them access the lower part of the extended space without any
    per driver change, but let the features field initializers set the
    extended space.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- hw/virtio/virtio-bus.c
+++ hw/virtio/virtio-bus.c
@@ -63,8 +63,8 @@ void virtio_bus_device_plugged(VirtIODevice *vdev, Error **errp)
 
     /* Get the features of the plugged device. */
     assert(vdc->get_features != NULL);
-    vdev->host_features = vdc->get_features(vdev, vdev->host_features,
-                                            &local_err);
+    vdev->host_features_ex = vdc->get_features(vdev, vdev->host_features,
+                                               &local_err);
     if (local_err) {
         error_propagate(errp, local_err);
         return;
