d48ef544c108cd74bad37dea77e75f90f97d5ef0
Paolo Abeni <pabeni@redhat.com>
2025-05-12 11:24:10 +0200

    virtio: add support for negotiating extended features.
    The virtio specifications allows for up to 128 bits for the
    device features. Soon we are going to use some of the 'extended'
    bits features (above 64) for the virtio net driver.
    
    Add support to allow extended features negotiation on a per
    devices basis. Devices willing to negotiated extended features
    need to implemented a new pair of features getter/setter, the
    core will conditionally use them instead of the basic one.
    
    Note that 'bad_features' don't need to be extended, as they are
    bound to the 64 bits limit.
    
    No functional changes intended for host without 128 bit support.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -3114,13 +3114,24 @@ static int virtio_set_features_nocheck(VirtIODevice *vdev,
                                        virtio_features_t val)
 {
     VirtioDeviceClass *k = VIRTIO_DEVICE_GET_CLASS(vdev);
-    bool bad = (val & ~(vdev->host_features)) != 0;
+    bool bad = (val & ~(vdev->host_features_ex)) != 0;
 
-    val &= vdev->host_features;
-    if (k->set_features) {
-        k->set_features(vdev, val);
+    val &= vdev->host_features_ex;
+#ifdef CONFIG_INT128
+    if (!k->set_features_ex) {
+        val = (uint64_t)val;
     }
-    vdev->guest_features = val;
+
+    if (k->set_features_ex) {
+        k->set_features_ex(vdev, val);
+    } else
+#endif
+    {
+        if (k->set_features) {
+            k->set_features(vdev, val);
+        }
+    }
+    vdev->guest_features_ex = val;
     return bad ? -1 : 0;
 }
 
@@ -3193,7 +3204,7 @@ virtio_set_features_ex_nocheck_maybe_co(VirtIODevice *vdev, __uint128_t val)
 
 #endif
 
-int virtio_set_features(VirtIODevice *vdev, uint64_t val)
+int virtio_set_features(VirtIODevice *vdev, virtio_features_t val)
 {
     int ret;
     /*
