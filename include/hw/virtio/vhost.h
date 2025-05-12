33ec2555321e465f9caf4b482cdac238e6c92ea7
Paolo Abeni <pabeni@redhat.com>
2025-05-12 12:20:00 +0200

    vhost: add support for negotiating extended features.
    Similar to virtio infra, vhost core maintain the features
    status in the widest format available and allow the devices
    to implement extended version of the getter/setter.
    
    Some care is needed for features bit manipulation: when clearing
    a bit with 'and not' bitwise operations, the bit mask must be
    extended to the feature format, or the all highest bits will be
    unintentionally cleared.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- include/hw/virtio/vhost.h
+++ include/hw/virtio/vhost.h
@@ -106,9 +106,9 @@ struct vhost_dev {
      * future use should be discouraged and the variable retired as
      * its easy to confuse with the VirtIO backend_features.
      */
-    uint64_t features;
-    uint64_t acked_features;
-    uint64_t backend_features;
+    virtio_features_t features;
+    virtio_features_t acked_features;
+    virtio_features_t backend_features;
 
     /**
      * @protocol_features: is the vhost-user only feature set by
@@ -116,7 +116,7 @@ struct vhost_dev {
      * negotiated if VHOST_USER_F_PROTOCOL_FEATURES has been offered
      * by the backend (see @features).
      */
-    uint64_t protocol_features;
+    virtio_features_t protocol_features;
 
     uint64_t max_queues;
     uint64_t backend_cap;
@@ -308,8 +308,9 @@ void vhost_virtqueue_mask(struct vhost_dev *hdev, VirtIODevice *vdev, int n,
  * is supported by the vhost backend (hdev->features), the supported
  * feature_bits and the requested feature set.
  */
-uint64_t vhost_get_features(struct vhost_dev *hdev, const int *feature_bits,
-                            uint64_t features);
+virtio_features_t vhost_get_features(struct vhost_dev *hdev,
+                                     const int *feature_bits,
+                                     virtio_features_t features);
 
 /**
  * vhost_ack_features() - set vhost acked_features
@@ -321,7 +322,7 @@ uint64_t vhost_get_features(struct vhost_dev *hdev, const int *feature_bits,
  * the backends advertised features and the supported feature_bits.
  */
 void vhost_ack_features(struct vhost_dev *hdev, const int *feature_bits,
-                        uint64_t features);
+                        virtio_features_t features);
 unsigned int vhost_get_max_memslots(void);
 unsigned int vhost_get_free_memslots(void);
 
